c1ccc42f764e4de24a7acea2ec6ff2fcaa522436
Shenin Hassan <khalby786@gmail.com>
2020-03-18 16:40:36 +0400

    Added auto-update from GitHub to Glitch code!

--- server.js
+++ server.js
@@ -12,6 +12,21 @@ app.get("/collection", function(request, response) {
   response.sendFile(__dirname + "/demo/collection/collection.html");
 });
 
+app.post('/git', (req, res) => {
+  // If event is "push"
+  if (req.headers['x-github-event'] == "push") {
+    cmd.run('chmod 777 git.sh'); /* :/ Fix no perms after updating */
+    cmd.get('./git.sh', (err, data) => {  // Run our script
+      if (data) console.log(data);
+      if (err) console.log(err);
+    });
+    cmd.run('refresh');  // Refresh project
+
+    console.log("> [GIT] Updated with origin/master");
+  }
+
+  return res.sendStatus(200); // Send back OK status
+});
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
